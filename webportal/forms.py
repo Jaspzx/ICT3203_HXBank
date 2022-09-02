bb7572f08ceeee28e9e99151f57bdf97e3ffb54e
john-kjj <2000819@sit.singaporetech.edu.sg>
2022-09-02 18:28:24 +0800

    2fa support

--- webportal/forms.py
+++ webportal/forms.py
@@ -3,15 +3,18 @@ from wtforms import StringField, TextAreaField, IntegerField, BooleanField, Pass
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from wtforms.widgets import PasswordInput
 
+
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(min=3, max=20)])
-    firstname = StringField("First Name", validators=[InputRequired(), Length(min=3, max=20)]) 
+    firstname = StringField("First Name", validators=[InputRequired(), Length(min=3, max=20)])
     lastname = StringField("Last Name", validators=[InputRequired(), Length(min=3, max=20)])
     address = StringField("Address", validators=[InputRequired(), Length(min=3, max=30)])
-    password = PasswordField("Password", validators=[InputRequired(), Length(min=8), EqualTo('confirm_password', message='Passwords must match')])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8),
+                                                     EqualTo('confirm_password', message='Passwords must match')])
     confirm_password = PasswordField("Repeat Password")
     email = StringField("Email", validators=[InputRequired(), Length(min=3, max=20), Email()])
     accept_tos = BooleanField("I accept the Terms & Conditions", validators=[InputRequired()])
+
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)],
@@ -19,3 +22,9 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=20)],
                              render_kw={"placeholder": "Password"})
     submit = SubmitField("Login")
+
+
+class Token2FAForm(FlaskForm):
+    token = StringField("2FA Token", validators=[InputRequired(), Length(min=6, max=6)],
+                        render_kw={"placeholder": "OTP Token"})
+    submit = SubmitField("Authenticate")
--- webportal/models/User.py
+++ webportal/models/User.py
@@ -1,35 +1,50 @@
 from flask_login import UserMixin
-from datetime import datetime 
+from datetime import datetime
 from webportal import db
+import pyotp
+
 
 class User(db.Model, UserMixin):
-	id = db.Column(db.INT, primary_key=True)
-	username = db.Column(db.String(50))
-	firstname = db.Column(db.String(50))
-	lastname = db.Column(db.String(50))
-	address = db.Column(db.String(50))
-	email = db.Column(db.String(150))
-	password_hash = db.Column(db.String(150))
-	date_joined = db.Column(db.DateTime())
+    id = db.Column(db.INT, primary_key=True)
+    username = db.Column(db.String(50))
+    firstname = db.Column(db.String(50))
+    lastname = db.Column(db.String(50))
+    address = db.Column(db.String(50))
+    email = db.Column(db.String(150))
+    password_hash = db.Column(db.String(150))
+    otp_secret = db.Column(db.String(16))
+    date_joined = db.Column(db.DateTime())
 
-	def __init__(self, username, firstname, lastname, address, email, password_hash): 
-		self.username = username
-		self.firstname = firstname
-		self.lastname = lastname
-		self.address = address
-		self.email = email
-		self.password_hash = password_hash
-		self.date_joined = datetime.now()
+    def __init__(self, username, firstname, lastname, address, email, password_hash, otp_secret):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.email = email
+        self.password_hash = password_hash
+        self.otp_secret = otp_secret
+        if self.otp_secret is None:
+            self.otp_secret = pyotp.random_base32()
+        self.date_joined = datetime.now()
 
-def createUser(username, firstname, lastname, address, email, password):
-	new_user = User(username, firstname, lastname, address, email, password)
-	try:
-		db.session.add(new_user)
-		db.session.commit()	
-	except:
-		db.session.rollback()
-	finally: 
-		db.session.close()    
+    def get_totp_uri(self):
+        return f'otpauth://totp/2FA-Demo:{self.username}?secret={self.otp_secret}&issuer=2FA-Demo'
+
+    def verify_totp(self, token):
+        totp = pyotp.TOTP(self.otp_secret)
+        return totp.verify(token)
+
+
+def createUser(username, firstname, lastname, address, email, password, otp_secret=None):
+    new_user = User(username, firstname, lastname, address, email, password, otp_secret)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
 
 def delUser():
-	pass 
+    pass
