2574ae46db120791b9b667fca3c8f23d6764697d
ryangohsc <32363441+ryangohsc@users.noreply.github.com>
2022-09-02 14:11:16 +0800

    Completed registration logic

--- webportal/forms.py
+++ webportal/forms.py
@@ -1,12 +1,15 @@
-from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField, RadioField)
-from wtforms.validators import InputRequired, Length
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, TextAreaField, IntegerField, BooleanField, PasswordField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.widgets import PasswordInput
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username')
-    firstname = StringField('First Name') 
-    lastname = StringField("Last Name")
-    address = StringField("Address")
-    password = StringField("Password")
-    email = StringField("Email")
-    
+    username = StringField("Username", validators=[InputRequired(), Length(min=3, max=20)])
+    firstname = StringField("First Name", validators=[InputRequired(), Length(min=3, max=20)]) 
+    lastname = StringField("Last Name", validators=[InputRequired(), Length(min=3, max=20)])
+    address = StringField("Address", validators=[InputRequired(), Length(min=3, max=30)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8), EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField("Repeat Password")
+    email = StringField("Email", validators=[InputRequired(), Length(min=3, max=20), Email()])
+    accept_tos = BooleanField("I accept the Terms & Conditions", validators=[InputRequired()])
+
--- webportal/views.py
+++ webportal/views.py
@@ -1,24 +1,32 @@
 from flask import Blueprint
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from .forms import RegisterForm
+from webportal.models.user import *
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html', title="Home Page")
+	return render_template('home.html', title="Home Page")
 
-@views.route('/register')
+@views.route('/register', methods=('GET', 'POST'))
 def register(): 
-    form = RegisterForm()
-    return render_template('register.html', title="Register", form=form)
+	form = RegisterForm()
+	if form.validate_on_submit():
+		username = form.username.data
+		firstname = form.firstname.data
+		lastname = form.lastname.data
+		address = form.address.data
+		email = form.email.data
+		password = form.password.data
+		createUser(username, firstname, lastname, address, email, password)       
+		return redirect(url_for("views.registration_successful"))
+	return render_template('register.html', title="Register", form=form)
 
-@views.route('/register_status', methods=('GET', 'POST'))
-def register_status():
-    form = RegisterForm()
-
-    return render_template('register_status', title="Register Status")
+@views.route("/registration_successful")
+def registration_successful():
+	return render_template('registration_successful.html', title="Registration Successful") 
 
 @views.route("/robots.txt")
 def robots():
-    return render_template('robots.txt', title="Robots")
+	return render_template('robots.txt', title="Robots")
