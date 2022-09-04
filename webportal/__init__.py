4be2ae1e24250425298538ed9c23aed95b1d454d
john-kjj <2000819@sit.singaporetech.edu.sg>
2022-09-04 21:59:33 +0800

    csrf protection

--- webportal/__init__.py
+++ webportal/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
 login_manager = LoginManager()
+csrf = CSRFProtect()
 DB_NAME = "database.db"
 
 from webportal.models.User import *
@@ -21,6 +23,7 @@ def create_webportal():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     flask_bcrypt.init_app(app)
+    csrf.init_app(app)
     login_manager.init_app(app)
     login_manager.session_protection = "strong"
     login_manager.login_view = 'views.login'
