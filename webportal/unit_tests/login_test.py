45358807026aee06a974bb475cf6c87fdc680844
yanjiex-x <fantasy.in.making@gmail.com>
2022-10-29 11:28:29 +0800

    Login Unit Testing

--- webportal/unit_tests/login_test.py
+++ webportal/unit_tests/login_test.py
@@ -2,14 +2,11 @@ import unittest
 from flask_bcrypt import Bcrypt
 from webportal import db, create_webportal, create_test_webportal
 from webportal.controllers.AccountManagementController import *
-from webportal.controllers.BankAccountManagementController import *
 
 app = create_test_webportal()
 
 class LoginTest(unittest.TestCase):
     amc = AccountManagementController()
-    bamc = BankAccountManagementController()
-    acc_number1 = ""
 
     def setUp(self):
         password1 = flask_bcrypt.generate_password_hash("password1")
@@ -17,7 +14,6 @@ class LoginTest(unittest.TestCase):
             db.create_all()
             self.amc.add_user("test", "Raymond", "Tan", "10kkj", "raymondtan@gmail.com", "98765433", "S12341235",
                          "11-11-1111", password1, None, None, 0)
-            self.acc_number1, welcome_amt1 = self.bamc.add_bank_account(User.query.filter_by(username="test").first().id)
 
     def testInit(self):
         with app.app_context():
