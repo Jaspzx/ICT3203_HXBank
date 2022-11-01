b09080061d0444014950f65b44afde16b6315760
EddieTanDJ <2002226@sit.singaporetech.edu.sg>
2022-11-01 12:49:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from flask import Flask
 app = create_webportal()
 
 if __name__ == "__main__":
-    app.run(host="localhost", port="5000", debug=True)
+    app.run(host="localhost", port="5000")
 
 
